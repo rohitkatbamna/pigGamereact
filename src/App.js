3c4f55e77c4ad9e7ca70c479b376844df564e6b2
rohit katbamna <rohitkatbamna@protonmail.com>
2022-02-28 19:02:42 +0530

    28TH FEB 07:02 PM 1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+const randomNumber = Math.floor(Math.random() * 6) + 1;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [playerone, setPlayerone] = useState(0);
+	const [playertwo, setPlayertwo] = useState(0);
+	console.log(randomNumber);
+	return (
+		<div
+			className="container"
+			style={{ marginRight: "5vw", marginLeft: "5vw" }}>
+			<div className="row text-center">
+				<div
+					className="col-5"
+					style={{ border: "2px solid black", backgroundColor: "lightblue" }}>
+					<h1>Player 1</h1>
+				</div>
+				<div className="col-2 fs-1 mb-auto">
+					<img src={"/" + randomNumber + ".svg"} height="75" width="75" />
+				</div>
+				<div
+					className="col-5"
+					style={{ border: "2px solid black", backgroundColor: "lightgreen" }}>
+					<h1>Player 2</h1>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
